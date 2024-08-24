32325fbfaa26a41a0a6f502d6ef2ac13268cf4ec
Baptiste Dauphouy <bdauphouy@gmail.com>
2024-08-24 17:44:51 +0200

    feat: landing page

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -5,4 +5,19 @@
 	export let data: PageData;
 </script>
 
+<div class="pb-80 pt-16 sm:pb-40 sm:pt-24 lg:pb-48 lg:pt-40">
+	<div class="relative mx-auto max-w-7xl sm:static">
+		<div class="sm:max-w-lg">
+			<h1 class="text-4xl font-bold tracking-tight text-gray-900 sm:text-6xl">
+				Welcome to this example store
+			</h1>
+			<p class="mt-4 text-xl text-gray-500">
+				Lorem ipsum dolor sit, amet consectetur adipisicing elit. Totam esse atque fugit eum
+				suscipit necessitatibus cupiditate quidem quam. Sit, maxime deleniti. Placeat corporis
+				laboriosam voluptas!
+			</p>
+		</div>
+	</div>
+</div>
+
 <ProductList title="Products" products={data.products} />
